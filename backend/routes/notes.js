326c9936d6aa647a8efbc4e91689302e70ab183f
KavinduDevinda97 <kavindudevinda9@gmail.com>
2026-03-05 11:31:01 +0530

    testing one

---
+++ backend/routes/notes.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const { getNotes, getNoteById, createNote, updateNote, deleteNote, addCollaborator } = require('../controllers/notesController');
+const { protect } = require('../middleware/auth');
+
+router.use(protect); // All note routes require authentication
+
+router.route('/')
+    .get(getNotes)
+    .post(createNote);
+
+router.route('/:id')
+    .get(getNoteById)
+    .put(updateNote)
+    .delete(deleteNote);
+
+router.post('/:id/collaborators', addCollaborator);
+
+module.exports = router;
